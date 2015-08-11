886d9c6e6452bb8bf973f6cedd7e499eb2e3cea3
joshua <joshua@tmon.co.kr>
2015-08-11 17:10:00 +0900

    AsyncService for Lists

--- src/main/java/com/tmoncorp/mobile/util/spring/async/AsyncService.java
+++ src/main/java/com/tmoncorp/mobile/util/spring/async/AsyncService.java
@@ -59,63 +59,116 @@ public class AsyncService {
 	public <T> Future<T> submitAsync(Callable<T> call){
 		return pool.submit(call);
 	}
-	
-	public <T,K> List<T> processAsyncList(List<K> datas,AsyncTask<K,T> call){
-		Vector<T> list = new Vector<T>();
+
+	private <T,K> Vector<Future<T>> requestAsyncList(List<K> datas,AsyncTask<K,T> call){
 		Vector<Future<T>> futureList=new Vector<>();
 
 		final ClientInfo info=ClientInfoService.getInfo();
-		
-		for (K data : datas) {
-			
-			Future<T> f=submitAsync(new Callable<T>(){
 
+		for (K data : datas) {
+
+			Future<T> f=submitAsync(new Callable<T>(){
 				@Override
-                public T call() throws Exception {
+				public T call() throws Exception {
 					ClientInfoService.setInfo(info);
 					T result=call.async(data);
 					ClientInfoService.clean();
-	                return result;
-                }});
+					return result;
+				}});
 			futureList.add(f);
-			
+
 		}
-		
+		return futureList;
+	}
+
+	private <T> List<T> gatherList(Vector<Future<T>> futureList){
+		Vector<T> list = new Vector<>();
 		int tasktime=0;
 		while(futureList.size() > 0){
-			
+
 			Future<T> m=futureList.firstElement();
 			if (m.isDone()){
 				T item;
-                try {
-	                item = m.get();
-	                if (item != null)
-	                	list.add(item);
-                } catch (InterruptedException e) {
-                	LOG.debug("Async Task interrupted {} ",e);
-	                break;
-                } catch (ExecutionException e) {
-                	LOG.debug("Async Task excution exception {} ",e);
-	                continue;
-                } catch (RuntimeException e){
-	                LOG.debug("Async Task runtime exception {} ",e);
-	                continue;
-                }
-                futureList.remove(m);
-                
+				try {
+					item = m.get();
+					if (item != null)
+						list.add(item);
+				} catch (InterruptedException e) {
+					LOG.debug("Async Task interrupted {} ",e);
+					break;
+				} catch (ExecutionException e) {
+					LOG.debug("Async Task excution exception {} ",e);
+					continue;
+				} catch (RuntimeException e){
+					LOG.debug("Async Task runtime exception {} ",e);
+					continue;
+				}
+				futureList.remove(m);
+
 			}else{
 				if (TASK_TIMEOUT < tasktime)
 					break;
 				try {
 					tasktime+=TASK_DELAY;
-		            Thread.sleep(TASK_DELAY);
+					Thread.sleep(TASK_DELAY);
 				} catch (InterruptedException e) {
-	            	LOG.debug("Async Task loop interrupted {} ",e);
-		            break;
-	            }
+					LOG.debug("Async Task loop interrupted {} ",e);
+					break;
+				}
 			}
 		}
 		return list;
+	}
+
+	private <T> List<T> mergeListOrder(Vector<Future<List<T>>> futureList){
+		Vector<T> list = new Vector<>();
+		int tasktime=0;
+		while(futureList.size() > 0){
+
+			Future<List<T>> m=futureList.firstElement();
+			if (m.isDone()){
+				List<T> item;
+				try {
+					item = m.get();
+					if (item != null)
+						list.addAll(item);
+				} catch (InterruptedException e) {
+					LOG.debug("Async Task interrupted {} ",e);
+					break;
+				} catch (ExecutionException e) {
+					LOG.debug("Async Task excution exception {} ",e);
+					continue;
+				} catch (RuntimeException e){
+					LOG.debug("Async Task runtime exception {} ",e);
+					continue;
+				}
+				futureList.remove(m);
+
+			}else{
+				if (TASK_TIMEOUT < tasktime)
+					break;
+				try {
+					tasktime+=TASK_DELAY;
+					Thread.sleep(TASK_DELAY);
+				} catch (InterruptedException e) {
+					LOG.debug("Async Task loop interrupted {} ",e);
+					break;
+				}
+			}
+		}
+		return list;
+	}
+	
+	public <T,K> List<T> processAsyncList(List<K> datas,AsyncTask<K,T> call){
+
+		Vector<Future<T>> futureList=requestAsyncList(datas,call);
+		return gatherList(futureList);
+	}
+
+	public <K,T> List<T> processAsyncMergeList(List<K> datas,AsyncTask<K,List<T>> call){
+
+		Vector<Future<List<T>>> futureList=requestAsyncList(datas,call);
+		return mergeListOrder(futureList);
 	}
 	
 	
