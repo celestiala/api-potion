e97250c07599fd8aa1844825bd876122f0baea94
joshua <joshua@tmon.co.kr>
2015-08-12 14:57:26 +0900

    default client for test code or etc

--- src/main/java/com/tmoncorp/mobile/util/spring/clientinfo/ClientInfoService.java
+++ src/main/java/com/tmoncorp/mobile/util/spring/clientinfo/ClientInfoService.java
@@ -1,13 +1,24 @@
 package com.tmoncorp.mobile.util.spring.clientinfo;
 
 import com.tmoncorp.mobile.util.common.clientinfo.ClientInfo;
+import com.tmoncorp.mobile.util.common.clientinfo.ClientPlatform;
 
 public class ClientInfoService {
+
+	private static final ClientInfo defaultInfo;
+
+	static {
+		defaultInfo=new ClientInfo();
+		defaultInfo.setPlatform(ClientPlatform.MOBILE);
+	}
 	
-	private static final ThreadLocal<ClientInfo> threadLocal = new ThreadLocal<ClientInfo>();
+	private static final ThreadLocal<ClientInfo> threadLocal = new ThreadLocal<>();
 	
 	public static ClientInfo getInfo(){
-		return threadLocal.get();
+		ClientInfo info=threadLocal.get();
+		if (info == null)
+			info=defaultInfo;
+		return info;
 	}
 	
 	public static void setInfo(ClientInfo info){
